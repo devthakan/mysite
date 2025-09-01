9aecf856fc286f01b33b9619a5ffb8a447722a6d
devthakan <thakandev2@gmail.com>
2025-09-01 08:46:21 +0530

    Update script.js

--- alldata/script.js
+++ alldata/script.js
@@ -1,6 +1,6 @@
-// आपकी Supabase प्रोजेक्ट की जानकारी
+// आपकी Supabase प्रोजेक्ट की जानकारी (सही API Key के साथ)
 const SUPABASE_URL = 'https://sjfglhxjdyvcygunijvz.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzII1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZmdsaHhqZHl2Y3lndW5panZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNDkyMDcsImV4cCI6MjA3MDgyNTIwN30.HduOuf_wdZ4iHHNN26ECilX_ALCHfnPPC07gYPN2tsM';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNqZmdsaHhqZHl2Y3lndW5panZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyNDkyMDcsImV4cCI6MjA3MDgyNTIwN30.HduOuf_wdZ4iHHNN26ECilX_ALCHfnPPC07gYPN2tsM';
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
